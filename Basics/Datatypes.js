cacfa7a7c51d2c2cdfffaa094d025c5f88851212
Achyuta Pradhan <129358396+Achyutapradhan@users.noreply.github.com>
2024-01-24 05:50:53 +0000

    Data Types ki kahani khatam

---
+++ Basics/Datatypes.js
@@ -0,0 +1,17 @@
+//number=> range 2 to the power 53
+//bigint
+//string =>""
+// boolean=> true/false
+//null=> standalone value
+//undefined
+//symbol => to definevuniqueness
+//object
+
+// we can check the data types of the given value by using "type"
+//ex:
+let age=23;
+console.log(typeof object) //undefined
+console.log(typeof age)//number
+console.log(typeof null) //object (special case and interview question)
+console.log(typeof "Dudul") //string
+console.log(typeof undefined) //undefined
